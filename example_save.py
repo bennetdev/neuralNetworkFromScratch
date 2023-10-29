0df41db5846ba73219b6dc91666ebafa596cb68f
bennetdev <bennetdevelope@gmail.com>
2023-10-29 13:35:29 +0100

    Bias update #1
    added bias for hidden layers (seems to be to complex for this issue) and comments

--- example_save.py
+++ example_save.py
@@ -14,7 +14,7 @@ hidden_nodes = 100
 output_nodes = 10
 
 # learning rate
-learning_rate = 0.1
+learning_rate = 0.001
 
 # create instance of neural network
 n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
@@ -24,10 +24,10 @@ training_data_file = open("data/mnist_train.csv", 'r')
 training_data_list = training_data_file.readlines()
 training_data_file.close()
 
-epochs = 3
+epochs = 15
 
+nextProgressPrint = 0.05
 for e in range(epochs):
-    nextProgressPrint = 0.05
     # go through all records in the training data set
     for i, record in enumerate(training_data_list):
         progress = (i + 1 + len(training_data_list) * e) / (len(training_data_list) * epochs)
@@ -73,7 +73,33 @@ for record in test_data_list:
         # network's answer doesn't match correct answer, add 0 to scorecard
         scorecard.append(0)
 
+# scorecard for how well the network performs, initially empty
+scorecardTrain = []
+
+# go through all the records in the test data set
+for record in training_data_list:
+    # split the record by the ',' commas
+    all_values = record.split(',')
+    # correct answer is first value
+    correct_label = int(all_values[0])
+    # scale and shift the inputs
+    inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    # query the network
+    outputs = n.query(inputs)
+    # the index of the highest value corresponds to the label
+    label = np.argmax(outputs)
+    # append correct or incorrect to list
+    if (label == correct_label):
+        # network's answer matches correct answer, add 1 to scorecard
+        scorecardTrain.append(1)
+    else:
+        # network's answer doesn't match correct answer, add 0 to scorecard
+        scorecardTrain.append(0)
+
 # calculate the performance score, the fraction of correct answers
 scorecard_array = np.asarray(scorecard)
-print("performance = ", scorecard_array.sum() / scorecard_array.size)
-n.save()
+scorecard_array_train = np.asarray(scorecardTrain)
+print("performanceTest = ", scorecard_array.sum() / scorecard_array.size)
+print("performanceTrain = ", scorecard_array_train.sum() / scorecard_array_train.size)
+if input("Do you want to save the model?[Y/N] ") == "Y":
+    n.save()
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -15,7 +15,6 @@ import random
 # - weights between hidden-layer and output-layer
 # - learning rate
 # - activation function to use across neural network
-# TODO bias and neuroevolution not working with multiple hidden layers
 class NeuralNetwork:
     def __init__(self, inputnodes: int, hiddennodes: Union[int, List[int]], outputnodes: int, learningrate: float):
         # Nodes are initialized as integers
@@ -32,118 +31,147 @@ class NeuralNetwork:
 
         # init biases
         self.biasH = np.random.normal(-1, 1, (self.hnodes[0], 1))
+        self.biasesH = []
         self.biasO = np.random.normal(-1, 1, (self.onodes, 1))
 
         # if there are multiple hidden layers
         if len(self.hnodes) > 1:
             self._generate_hidden_weights()
+            self._generate_hidden_biases()
 
         self.activation_function = lambda x: scipy.special.expit(x)
 
 
     # Generates the weights between hidden layers if there are multiple hidden layers
     def _generate_hidden_weights(self) -> None:
-        #print(self.hnodes)
         for index in range(0, len(self.hnodes) - 1):
             weights = np.random.normal(0.0, pow(self.hnodes[index], -0.5),
                                           (self.hnodes[index], self.hnodes[index + 1])).tolist()
             self.whh.append(weights)
         self.whh = np.array(self.whh)
-        #print(self.whh)
+
+
+    # Generates the biases for every hidden layer if there are multiple hidden layers
+    def _generate_hidden_biases(self) -> None:
+        # generate one bias for each layer
+        for index in range(0, len(self.hnodes) - 1):
+            bias = np.random.normal(-1, 1, (self.hnodes[index + 1], 1))
+            self.biasesH.append(bias)
+        self.biasesH = np.array(self.biasesH)
+
 
     # train the neural network
     # Input: input data, Numpy ndarray
     #        Target data, Numpy dnarray
     def train(self, inputs_list: np.ndarray, targets_list: np.ndarray) -> None:
         # convert inputs list to 2d array
-        inputs = np.array(inputs_list, ndmin=2).T
-        targets = np.array(targets_list, ndmin=2).T
+        inputsToNeuralNetwork = np.array(inputs_list, ndmin=2).T
+        targetOutputs = np.array(targets_list, ndmin=2).T
 
         # calculate signals into hidden layer
-        hidden_inputs = np.dot(self.wih, inputs)
+        inputsToHidden = np.dot(self.wih, inputsToNeuralNetwork)
         biasHCopy = self.biasH.copy()
         np.concatenate((biasHCopy, self.biasH), axis=1)
-        hidden_inputs += biasHCopy
+        inputsToHidden += biasHCopy
         # calculate the signals emerging from hidden layer
-        hidden_outputs = self.activation_function(hidden_inputs)
+        outputsFromFirstHidden = self.activation_function(inputsToHidden)
 
-        outputs = [hidden_outputs]
-        hidden_layer_outputs = hidden_outputs
+        outputsFromAllHidden = [outputsFromFirstHidden]
+        outputsFromLastHidden = outputsFromFirstHidden
         for index, hidden_layer in enumerate(self.whh):
-            hidden_layer_inputs = np.dot(hidden_layer, hidden_layer_outputs)
-            hidden_layer_outputs = self.activation_function(hidden_layer_inputs)
-            outputs.append(hidden_layer_outputs.tolist())
-        outputs = np.array(outputs)
+            inputsToCurrentHidden = np.dot(hidden_layer, outputsFromLastHidden)
+            # add bias to input
+            biasCopy = self.biasesH[index].copy()
+            np.concatenate((biasCopy, self.biasesH[index]), axis=1)
+            inputsToCurrentHidden += biasCopy
+
+            outputsFromLastHidden = self.activation_function(inputsToCurrentHidden)
+            outputsFromAllHidden.append(outputsFromLastHidden.tolist())
+        outputsFromAllHidden = np.array(outputsFromAllHidden)
 
         # calculate signals into final output layer
-        final_inputs = np.dot(self.who, hidden_layer_outputs)
+        inputsToOutput = np.dot(self.who, outputsFromLastHidden)
         biasOCopy = self.biasO.copy()
         np.concatenate((biasOCopy, self.biasO), axis=1)
-        final_inputs += biasOCopy
+        inputsToOutput += biasOCopy
         # calculate the signals emerging from final output layer
-        final_outputs = self.activation_function(final_inputs)
+        final_outputs = self.activation_function(inputsToOutput)
         # print(final_outputs)
 
         # output layer error is the (target - actual)
-        output_errors = targets - final_outputs
+        output_errors = targetOutputs - final_outputs
         # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
         hidden_errors = np.dot(self.who.T, output_errors)
 
         hidden_layer_errors = hidden_errors
-        errors = [hidden_errors]
+        errorsFromAllHidden = [hidden_errors]
         for hidden_layer in self.whh:
             hidden_layer_errors = np.dot(hidden_layer.T, hidden_layer_errors)
-            errors.append(hidden_layer_errors.tolist())
-        errors = np.array(errors)
+            errorsFromAllHidden.append(hidden_layer_errors.tolist())
+        errorsFromAllHidden = np.array(errorsFromAllHidden)
 
         # update the weights for the links between the hidden and output layers
         self.who += self.lr * np.dot((output_errors * final_outputs * (1.0 - final_outputs)),
-                                        np.transpose(hidden_layer_outputs))
+                                      np.transpose(outputsFromLastHidden))
+        
+        # update weights for the links between hidden layers and biases for hidden layers
         for index, hidden_layer in enumerate(self.whh):
             self.whh[index] += self.lr * np.dot(
-                (errors[index + 1] * outputs[index + 1] * (1.0 - outputs[index + 1])),
-                np.transpose(outputs[index]))
+                (errorsFromAllHidden[index + 1] * outputsFromAllHidden[index + 1] * (1.0 - outputsFromAllHidden[index + 1])),
+                np.transpose(outputsFromAllHidden[index]))
+            
+            # update biases
+            self.biasesH[index] += (errorsFromAllHidden[index + 1]  * (1.0 - outputsFromAllHidden[index + 1])) * self.lr
+        
         # update the weights for the links between the input and hidden layers
-        self.wih += self.lr * np.dot((errors[0] * outputs[0] * (1.0 - outputs[0])),
-                                        np.transpose(inputs))
+        self.wih += self.lr * np.dot((errorsFromAllHidden[0] * outputsFromAllHidden[0] * (1.0 - outputsFromAllHidden[0])),
+                                        np.transpose(inputsToNeuralNetwork))
 
         # update biases
         self.biasO += (output_errors  * (1.0 - final_outputs)) * self.lr
-        self.biasH += (hidden_errors * (1.0 - hidden_outputs)) * self.lr
+        self.biasH += (hidden_errors * (1.0 - outputsFromFirstHidden)) * self.lr
+
 
     # query the neural network for specific input
     # Input: List of input values, np.ndarray with only one dimension (gets converted to 2d array)
     # Output: List of all possible outputs with possibility, Numpy
     def query(self, inputs_list: np.ndarray) -> np.ndarray:
         # convert inputs list to 2d array
-        inputs = np.array(inputs_list, ndmin=2).T
+        inputsToNeuralNetwork = np.array(inputs_list, ndmin=2).T
 
         # calculate signals into hidden layer
-        hidden_inputs = np.dot(self.wih, inputs)
+        inputsToHidden = np.dot(self.wih, inputsToNeuralNetwork)
         # add bias to hidden_inputs
         biasHCopy = self.biasH.copy()
         np.concatenate((biasHCopy, self.biasH), axis=1)
-        hidden_inputs += biasHCopy
+        inputsToHidden += biasHCopy
 
         # calculate the signals emerging from hidden layer
-        hidden_outputs = self.activation_function(hidden_inputs)
+        outputsFromFirstHidden = self.activation_function(inputsToHidden)
 
         # only executes with multiple hidden layers
-        hidden_layer_outputs = hidden_outputs
+        outputsFromLastHidden = outputsFromFirstHidden
+        # TODO zip?
         for index, hidden_layer in enumerate(self.whh):
-            hidden_layer_inputs = np.dot(hidden_layer, hidden_layer_outputs)
-            hidden_layer_outputs = self.activation_function(hidden_layer_inputs)
+            inputsToCurrentHidden = np.dot(hidden_layer, outputsFromLastHidden)
+            # add bias to input
+            biasCopy = self.biasesH[index].copy()
+            np.concatenate((biasCopy, self.biasesH[index]), axis=1)
+            inputsToCurrentHidden += biasCopy
+
+            outputsFromLastHidden = self.activation_function(inputsToCurrentHidden)
 
         # calculate signals into final output layer
-        final_inputs = np.dot(self.who, hidden_layer_outputs)
-        # add bias to final_inputs
+        inputsToOutput = np.dot(self.who, outputsFromLastHidden)
+        # add bias to inputsToOutput
         biasOCopy = self.biasO.copy()
         np.concatenate((biasOCopy, self.biasO), axis=1)
-        final_inputs += biasOCopy
+        inputsToOutput += biasOCopy
         # calculate the signals emerging from final output layer
-        final_outputs = self.activation_function(final_inputs)
+        final_outputs = self.activation_function(inputsToOutput)
 
         return final_outputs
+
 
     # save neural network to npy files and metadata to json
     def save(self) -> None:
@@ -152,6 +180,7 @@ class NeuralNetwork:
         np.save("save/who.npy", self.who)
         np.save("save/biasH.npy", self.biasH)
         np.save("save/biasO.npy", self.biasO)
+        np.save("save/biasesH.npy", self.biasesH)
         with open("save/metadata.json", "w") as file:
             json.dump({
                 "inputnodes": self.inodes,
@@ -172,9 +201,11 @@ class NeuralNetwork:
         copy.wih = self.wih.copy()
         copy.who = self.who.copy()
         copy.biasH = self.biasH.copy()
+        copy.biasesH = self.biasesH.copy()
         copy.biasO = self.biasO.copy()
 
         return copy
+
 
     # Function to create child out of two parent neural networks (including self) by crossing over their weights
     # This implementation chooses every weight off a 50/50 chance, TODO only for one hidden layer right now
@@ -221,14 +252,15 @@ class NeuralNetwork:
         self.biasO = changeValueFunction(self.biasO)
         self.biasH = changeValueFunction(self.biasH)
 
+
 # Load neural network from npy files and json
 # Output: Neural Network with metadata and weights from files
-# TODO bias
 def load() -> NeuralNetwork:
     wih = np.load("save/wih.npy")
     whh = np.load("save/whh.npy")
     who = np.load("save/who.npy")
     biasH = np.load("save/biasH.npy")
+    biasesH = np.load("save/biasesH.npy")
     biasO = np.load("save/biasO.npy")
     with open("save/metadata.json", "r") as file:
         metadata = json.load(file)
@@ -238,5 +270,6 @@ def load() -> NeuralNetwork:
         n.whh = whh
         n.who = who
         n.biasH = biasH
+        n.biasesH = biasesH
         n.biasO = biasO
     return n
